d2aeb170d891dd9cd7577e8394a6dd5651ec5c0f
Yeabsra-Seyoum <Yeasbra.s.hailu97@gmail.com>
2026-06-04 22:15:41 +0300

    feat: add graph visualiser

--- Gready Best-First Search.py
+++ Gready Best-First Search.py
@@ -66,3 +66,84 @@ def calculate_path_cost(path, edge_weights):
         elif (v, u) in edge_weights:
             total_cost += edge_weights[(v, u)]
     return total_cost
+
+#Visualizer
+
+
+
+
+def visualize_graph(graph, edge_weights, path, pos):
+    G = nx.Graph()
+
+    for node, neighbors in graph.items():
+        for neighbor in neighbors:
+            if (node, neighbor) in edge_weights:
+                G.add_edge(node, neighbor, weight=edge_weights[(node, neighbor)])
+            elif (neighbor, node) in edge_weights:
+                G.add_edge(node, neighbor, weight=edge_weights[(neighbor, node)])
+
+    plt.figure(figsize=(12, 7))
+
+    color_map = {
+        'Bole': '#a1d99b',    # Light Green
+        'Arada': '#bdd7e7',   # Light Blue
+        'Lafto': '#fddaec',   # Pale Yellow/Beige
+        'Yeka': '#fbb4ae',    # Pink
+        'Kirkos': '#cbd5e8',  # Purple
+        'Lideta': '#ccebc5',  # Cyan/Teal
+        'Kolfe': '#decbe4',   # Periwinkle Blue
+        'Gulele': '#fed9a6',  # Peach/Tan
+        'Jemo': '#b3cde3',    # Mint Green
+        'Akaky': '#fbb4ae'    # Red/Coral
+    }
+    
+    # alternative fallback
+    node_colors = [
+        '#adebad' if n == 'Bole' or n == 'Jemo' else
+        '#b3d9ff' if n == 'Arada' else
+        '#ffe699' if n == 'Lafto' or n == 'Gulele' else
+        '#ffb3d9' if n == 'Yeka' else
+        '#ccb3ff' if n == 'Kirkos' else
+        '#b3f0ff' if n == 'Lideta' else
+        '#cce0ff' if n == 'Kolfe' else
+        '#ff8080' if n == 'Akaky' else '#skyblue'
+        for n in G.nodes()
+    ]
+
+    # Draw edges
+    nx.draw_networkx_edges(G, pos, edge_color="black", width=1.2)
+
+    # Draw nodes
+    nx.draw_networkx_nodes(
+        G,
+        pos,
+        nodelist=list(G.nodes()),
+        node_color=node_colors,
+        node_size=2500,
+        edgecolors='black',
+        linewidths=1
+    )
+
+    nx.draw_networkx_labels(G, pos, font_size=11, font_weight="bold", font_color="black")
+
+    # Draw Edge Weights 
+    labels = nx.get_edge_attributes(G, 'weight')
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=labels, font_size=11, font_weight="bold")
+
+    if path:
+        path_edges = list(zip(path, path[1:]))
+        nx.draw_networkx_edges(
+            G,
+            pos,
+            edgelist=path_edges,
+            edge_color="#2ca02c",  
+            width=4.5
+        )
+
+    plt.text(pos['Bole'][0], pos['Bole'][1] + 0.4, "Start Node", color="green", weight="bold", fontsize=12, ha="center")
+    plt.text(pos['Akaky'][0], pos['Akaky'][1] - 0.4, "Goal Node", color="red", weight="bold", fontsize=12, ha="center")
+
+    plt.title("Greedy Best-First Search", fontsize=14, weight="bold")
+    plt.axis('off')
+    plt.tight_layout()
+    plt.show()
