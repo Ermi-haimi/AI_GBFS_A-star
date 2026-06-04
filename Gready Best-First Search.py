55632341029076c56a893f3b71efa89162a423f4
Yeabsra-Seyoum <Yeasbra.s.hailu97@gmail.com>
2026-06-04 22:14:35 +0300

    feat: implement path construction & path cost calculation

--- Gready Best-First Search.py
+++ Gready Best-First Search.py
@@ -41,3 +41,28 @@ def greedy_best_first_search(graph, start, goal, heuristic):
                     path[neighbor] = current_node
 
     return None, expansion_order
+
+
+# Reconstruct path
+def reconstruct_path(path, start, goal):
+    current = goal
+    result_path = []
+
+    while current is not None:
+        result_path.append(current)
+        current = path[current]
+
+    result_path.reverse()
+    return result_path
+
+def calculate_path_cost(path, edge_weights):
+    if not path:
+        return 0
+    total_cost = 0
+    for i in range(len(path) - 1):
+        u, v = path[i], path[i+1]
+        if (u, v) in edge_weights:
+            total_cost += edge_weights[(u, v)]
+        elif (v, u) in edge_weights:
+            total_cost += edge_weights[(v, u)]
+    return total_cost
