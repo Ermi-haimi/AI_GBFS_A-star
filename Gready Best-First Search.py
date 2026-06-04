bf903fbcacfa24bc4941f7abd8089984abdbaca5
Yeabsra-Seyoum <Yeasbra.s.hailu97@gmail.com>
2026-06-04 21:43:10 +0300

    feat: add node class

---
+++ Gready Best-First Search.py
@@ -0,0 +1,12 @@
+import heapq
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# Node representation
+class Node:
+    def __init__(self, name, heuristic):
+        self.name = name
+        self.heuristic = heuristic
+
+    def __lt__(self, other):
+        return self.heuristic < other.heuristic
