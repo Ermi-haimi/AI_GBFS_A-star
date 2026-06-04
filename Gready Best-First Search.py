9a3bac273b1c70eac8c9fed854e2464a6cac08d4
Yeabsra-Seyoum <Yeasbra.s.hailu97@gmail.com>
2026-06-04 21:44:13 +0300

    feat: implement GBFS algorithm

--- Gready Best-First Search.py
+++ Gready Best-First Search.py
@@ -10,3 +10,34 @@ class Node:
 
     def __lt__(self, other):
         return self.heuristic < other.heuristic
+
+
+
+# Greedy Best First Search
+def greedy_best_first_search(graph, start, goal, heuristic):
+    priority_queue = []
+    heapq.heappush(priority_queue, Node(start, heuristic[start]))
+
+    visited = set()
+    path = {start: None}
+    expansion_order = []
+
+    while priority_queue:
+        current_node = heapq.heappop(priority_queue).name
+
+        if current_node not in expansion_order:
+            expansion_order.append(current_node)
+
+        if current_node == goal:
+            return reconstruct_path(path, start, goal), expansion_order
+
+        visited.add(current_node)
+
+        for neighbor in graph[current_node]:
+            if neighbor not in visited:
+                heapq.heappush(priority_queue, Node(neighbor, heuristic[neighbor]))
+
+                if neighbor not in path:
+                    path[neighbor] = current_node
+
+    return None, expansion_order
